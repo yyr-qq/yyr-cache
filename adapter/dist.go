db3125f4ff73e79f57f716fdb69d059b4a967a27
tongtech@sina.com <>
2026-04-26 17:37:36 +0800

    修改模块名

--- adapter/dist.go
+++ adapter/dist.go
@@ -22,7 +22,7 @@ import (
 	"github.com/gogf/gf/v2/os/gcache"
 	"github.com/gogf/gf/v2/os/gtime"
 	"github.com/gogf/gf/v2/util/gconv"
-	"github.com/yyryydyyx/rs-cache/instance"
+	"github.com/yyr-qq/yyr-cache/instance"
 )
 
 const (
